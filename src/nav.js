87eaf77aedd9cf45542aa9e76e2346fb8437ef4a
tfmcmahon <tfmcmahon@gmail.com>
2022-08-31 15:53:28 -0500

    remove optinional chains for nav script

--- src/nav.js
+++ src/nav.js
@@ -1,7 +1,7 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav-link");
 
-navToggle?.addEventListener("click", () => {
+navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
 });
 
@@ -12,4 +12,4 @@ navLinks.forEach((link) => {
 });
 
 const fadeIn = document.querySelector(".fade-in");
-fadeIn?.classList.add("fade-in__active");
+fadeIn.classList.add("fade-in__active");
